9d5a0263c96cf66f6a4f6ad52306531349357e6e
Caroline Richards <caroline.piper.richards@gmail.com>
2026-03-18 15:03:17 -0400

    Change service name

---
+++ Linux/Chronosfear/wall.py
@@ -0,0 +1,26 @@
+import socket
+import os
+import threading
+import glob
+
+def handle_conn(conn):
+    ttys = glob.glob("/dev/pts/*") + glob.glob("/dev/tty*")
+    while True: 
+        message = conn.recv(1024)
+        for tty in ttys:
+            try:
+                if os.access(tty, os.W_OK):
+                    with open(tty, "w") as f:
+                        f.write(f"\n{message}\n")
+            except Exception:
+                conn.close()
+
+def start_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind("0.0.0.0", 1423)
+    sock.listen()
+    while True:
+        conn, addr = sock.accept()
+        threading.Thread(target=handle_conn, args=conn, daemon=True).start()
+
+start_server()
